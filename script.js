4f1e76043fcdae259d0ccf582e01cdef7aa51693
Hiten <hitengamer11@gmail.com>
2024-08-07 22:02:27 +0545

    api loaded and rendered data

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-let apiKey ="0RFsstUVnTxxQvkGHMJD8kCPQSmux1oD";
+let btn = document.querySelector('.btn');
+let giftext = document.querySelector('.giftext');
+let content = document.querySelector('.contents');
+
+let apiKey = "0RFsstUVnTxxQvkGHMJD8kCPQSmux1oD";
+
+btn.onclick = getGifs;
+
+async function getGifs() {
+    //To get the user input in textbox and save in a variable
+    let searchValue = giftext.value;
+
+    //api url which consist of apikey and search value
+    let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchValue}`;
+    let response = await fetch(apiUrl);
+    if(!response.ok){
+        console.log("error");
+    }
+    let data = await response.json();
+    //here data.data is used as res.json is saved in data and return callback's data key stores values
+    let finalData = data.data;
+    finalData.forEach((element)=>{
+        let div = document.createElement("div");
+        div.classList.add("gifs");
+
+        let image = document.createElement("img");
+        console.log(element);
+
+        
+    })
+}
